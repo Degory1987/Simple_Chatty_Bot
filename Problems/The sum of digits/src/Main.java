49b29e9be88d4f84d53646ebe89ec9f2fc41bce1
Degory1987 <73026633+Degory1987@users.noreply.github.com>
2020-10-25 10:02:24 +0100

    Update Main.java

--- Problems/The sum of digits/src/Main.java
+++ Problems/The sum of digits/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // put your code here
+        
         int number = scanner.nextInt();
         int a = number % 10;
         int b = number / 10 % 10;
